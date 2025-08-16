f00210fd03d4e3976b93c33e0134f3428d8c76c4
rajeshphy <bittuiitk@gmail.com>
2025-08-16 07:49:30 +0530

    Fellow Smith References Added

--- RECORDS/08_Codes/doi-scihub-download/dow.sh
+++ RECORDS/08_Codes/doi-scihub-download/dow.sh
@@ -7,7 +7,7 @@ mkdir -p "$pdf_dir"
 
 error_count=1
 
-while IFS=',' read -r journal title doi; do
+while IFS=',' read -r year title doi _; do
     url="https://sci-hub.se/${doi}"
     echo "Fetching: $title from $url"
 
@@ -24,11 +24,32 @@ while IFS=',' read -r journal title doi; do
                sed 's/src="//; s/^\/\///; s/^/https:\/\//')
 
     if [ -n "$pdf_link" ]; then
-        # Create safe PDF filename: JournalName - PaperTitle.pdf
-        pdf_name="${journal} - ${title}.pdf"
-        #pdf_name=$(echo "$pdf_name" | tr ' /:*?"<>|' '_' )
-        pdf_name=$(echo "$pdf_name" | tr ' /:*?\"<>|' '_' | sed -E -e 's/[;,]+/_/g' -e 's/_+/_/g' -e 's/([A-Za-z])_([A-Za-z])/\1\2/g' -e 's/[._-]+/-/g' -e 's/\)\^([0-9]+)/\1/g' -e 's/[()]//g' -e 's/^_+//; s/_+$//; s/^-+//; s/-+$//')
+    pdf_name="${year} - ${title}.pdf"
+    pdf_name=$(echo "$pdf_name" \
+      | sed -E 's/λ/lambda/g; s/Λ/Lambda/g; s/π/pi/g; s/Δ/Delta/g;' \
+      | sed -E 's/=/ -eq- /g; s/\+/ -plus- /g; s/−/ -minus- /g; s/–/ -minus- /g;' \
+      | tr ' /:*?"<>|' '_' \
+      | sed -E 's/\.[Pp][Dd][Ff]$/<<PDFEXT>>/;
+                s/[;,]+/-/g;
+                s/_+/-/g;
+                s/\b([A-Za-z])\-([A-Za-z])\b/\1\2/g;
+                s/\)\^([0-9]+)/\1/g;
+                s/[()]+//g;
+                s/[._-]+/-/g;
+                s/^_+//; s/_+$//; s/^-+//; s/-+$//;
+                s/<<PDFEXT>>$/.pdf/i')
+# Limit to 50 dash-separated words but always preserve .pdf
+pdf_name=$(echo "$pdf_name" | sed -E 's/\.pdf$//I')   # strip extension safely
+pdf_name=$(echo "$pdf_name" | awk -F'-' '{
+    max=20; out="";
+    for(i=1;i<=NF && i<=max;i++){
+        if(out=="") out=$i; else out=out"-"$i
+    }
+    print out (NF>max ? "-etc" : "")
+}')
+pdf_name="${pdf_name}.pdf"
 
+        
         # Try to download PDF
         if ! wget -q --show-progress "$pdf_link" -O "$pdf_dir/$pdf_name"; then
             # If failed, save as error-X.pdf
